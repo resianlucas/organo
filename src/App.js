2b6add6d4b8c6afb5815f5770294a3b85ee26381
resianlucas <rm95781@fiap.com.br>
2024-06-20 23:57:21 -0300

    inserindo rodape

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { useState } from 'react';
 import Banner from './componentes/Banner';
 import Formulario from './componentes/Formulario';
 import Time from './componentes/Time';
+import Rodape from './componentes/Rodape';
 
 function App() {
 
@@ -64,6 +65,7 @@ function App() {
         corSecundaria={time.corSecundaria} 
         colaboradores={colaboradores.filter(colaborador => colaborador.time === time.nome)}
       />)}
+      <Rodape />
     </div>
   );
 }
