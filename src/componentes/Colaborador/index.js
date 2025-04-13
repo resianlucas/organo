cd78ccccaf6ef0660a9dcdfdf9c1f5c655767a1c
resianlucas <rm95781@fiap.com.br>
2025-04-13 02:21:08 -0300

    criando botao ao deletar

--- src/componentes/Colaborador/index.js
+++ src/componentes/Colaborador/index.js
@@ -1,15 +1,19 @@
+import { IoIosCloseCircle } from "react-icons/io";
 import './colaborador.css'
 
-const Colaborador = ({ colaborador, corDeFundo }) => {
-    return (<div className="colaborador">
-        <div className="cabecalho" style={{ backgroundColor: corDeFundo }}>
-            <img src={colaborador.imagem} alt={colaborador.nome} />
+const Colaborador = ({ colaborador, corDeFundo, aoDeletar }) => {
+    return (
+        <div className="colaborador">
+            <IoIosCloseCircle size={25} className='deletar' onClick={aoDeletar} />
+            <div className="cabecalho" style={{ backgroundColor: corDeFundo }}>
+                <img src={colaborador.imagem} alt={colaborador.nome} />
+            </div>
+            <div className="rodape">
+                <h4>{colaborador.nome}</h4>
+                <h5>{colaborador.cargo}</h5>
+            </div>
         </div>
-        <div className="rodape">
-            <h4>{colaborador.nome}</h4>
-            <h5>{colaborador.cargo}</h5>
-        </div>
-    </div>)
+    )
 }
 
 export default Colaborador
